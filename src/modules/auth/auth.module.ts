f6e39a73a08644254f51c156651925642e8411b0
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-10-10 18:03:31 -0300

    JWT Strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,14 @@ import { UsersModule } from './../users/users.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UsersModule, AwsModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt ' }),
+    UsersModule,
+    AwsModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
