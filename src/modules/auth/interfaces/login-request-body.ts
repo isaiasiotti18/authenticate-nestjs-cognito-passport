530f38d94aed3c7df125f372d06491774258a417
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-10-13 13:57:52 -0300

    login request body

--- src/modules/auth/interfaces/login-request-body.ts
+++ src/modules/auth/interfaces/login-request-body.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginRequestBody {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
